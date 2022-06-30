3cde4cb325decf108d5a95d2d7bdfbc5d72a8b20
Ben <105577567+benjbright@users.noreply.github.com>
2022-06-30 16:14:07 +0100

    Initial commit
    Stephen Grider course - Ethereum and Solidity - Complete developer's guide.  Appendix - React walkthrough

---
+++ src/components/search_bar.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react"
+
+class SearchBar extends Component {
+  // State is a plain Javascript object that is used to record and react to user events
+  // Whenever a component's state is changed, the component immediately re-renders - also forces all of it's children to re-render
+
+  constructor(props) {
+    // All Javascript classes have a special function called the constructor
+    // the first and only function called automatically whenever a new instance of the class is created (e.g. whenever a new instance of SearchBar is created)
+    // reserved for set up inside of the class
+    // Component has its own constructor function - can access it by calling 'super'
+    super(props)
+
+    this.state = { term: "" }
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={(event) => this.setState({ term: event.target.value })}
+        />
+      </div>
+    )
+  }
+
+  // Add an event handler to the input element
+  // on or handle - name of element - name of the event itself
+  // use event object to access the value property of the input
+
+  // Controlled input / form element - value is set by the 'state'
+  // When tell the input that value is provided by state - controlled component
+  // Value set by state - value only changes when the state changes
+  // When the component re-renders, value of controlled component = state
+}
+
+export default SearchBar
--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from "react"
+import ReactDOM from "react-dom"
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from "./components/search_bar"
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = process.env.REACT_APP_YOUTUBE_TOKEN
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Create a new component - this should produce some HTML
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
+}
+
+// Take this component's generated HTML and put it on the page (in the DOM)
+// Pass an instance of the App component
+// Add a target DOM node
+ReactDOM.render(<App />, document.querySelector(".container"))
